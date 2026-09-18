588ef29bf0a1929c16d9525571d0ae87ecaece86
Lavale <lavaledegraffenreid@gmail.com>
2026-09-17 23:24:43 -0700

    Added RPN stack and basic commands

--- rpn-calculator-example.cpp
+++ rpn-calculator-example.cpp
@@ -48,12 +48,29 @@ uint8_t const width = 16U;
  * Students should create or add any functions or classes they may need.
  */
 shared_ptr<uint16_t> rpn_calc(command const cmd, uint16_t const value = 0) {
-    // this is example code which returns a (smart shared) pointer to 16-bit value
-    uint16_t val = 0b1001100100000011;
-    shared_ptr<uint16_t> result = make_shared<uint16_t>(val);
-    return result;
-}
+    static vector<uint16_t> stack;
 
+    switch (cmd) {
+
+        case cmd_enter:
+            stack.push_back(value);
+            return make_shared<uint16_t>(stack.back());
+
+        case cmd_clear:
+            stack.clear();
+            return nullptr;
+
+        case cmd_top:
+            if (stack.empty()) {
+                return nullptr;
+            } else {
+                return make_shared<uint16_t>(stack.back());
+            }
+
+        default:
+            return nullptr;
+    }
+}
 /*
  * *** STUDENTS SHOULD NOT NEED TO CHANGE THE CODE BELOW. IT IS A CUSTOM TEST HARNESS. ***
  */
